8faf1b353ee9d1958dcfdae38ee1a910b1cea90f
Stasyancho <stasyancho@gmail.com>
2025-01-10 23:08:15 +0300

    Added Enabled Area Generation

--- Assets/Scripts/AreaGenerater.cs
+++ Assets/Scripts/AreaGenerater.cs
@@ -1,114 +1,84 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
+using static PathGenerater;
 
-public static class PathGenerater
+public static class AreaGenerater
 {
-    static Dictionary<int, string> costs = new Dictionary<int, string>()
+    static void GetPieceOfMap(int[,] map, int x, int y, int radius, out int[,] newMap, out int[,] newMiniMap, out int x1, out int y1)
     {
-        [0] = "10",
-        [1] = "no",
-        [2] = "start",
-        [3] = "end"//,
-        //[1] = "start",
-        //[2] = "end",
-        //[3] = "no",
-        //[4] = "50",
-        //[5] = "30"
-    };
-    public static Cell[,] GetCellsFromMap(int[,] _map)
-    {
-        int minStepCost = Convert.ToInt32(costs[0]);
-        int n = _map.GetLength(0);
-        int m = _map.GetLength(1);
-        Cell[,] cells = new Cell[n, m];
+        x1 = x - radius >= 0 ? x - radius : 0;
+        int x2 = x + radius < map.GetLength(0) ? x + radius : map.GetLength(0) - 1;
 
-        int ii = 0;
-        int jj = 0;
+        y1 = y - radius >= 0 ? y - radius : 0;
+        int y2 = y + radius < map.GetLength(1) ? y + radius : map.GetLength(1) - 1;
 
-        for (int i = 0; i < n; i++)
+        newMiniMap = new int[x2 - x1 + 1, y2 - y1 + 1];
+        for (int i = 0; i < map.GetLength(0); i++)
         {
-            for (int j = 0; j < m; j++)
+            for (int j = 0; j < map.GetLength(1); j++)
             {
-                if (costs[_map[i, j]] == "end")
+                if (i == x && j == y)
                 {
-                    ii = i;
-                    jj = j;
+                    map[i, j] = 2;
+                }
+                if (i < x1 || i > x2 || j < y1 || j > y2)
+                {
+                    map[i, j] = 1;
+                }
+                else
+                {
+                    newMiniMap[i - x1, j - y1] = map[i, j];
                 }
             }
         }
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                switch (costs[_map[i, j]])
-                {
-                    case "start":
-                        cells[i, j] = new Cell()
-                        {
-                            cellCost = 0,
-                            distanceToEnd = (Math.Abs(i - ii) + Math.Abs(j - jj)) * minStepCost
-                        };
-                        break;
-                    case "end":
-                        cells[i, j] = new Cell()
-                        {
-                            stepCost = minStepCost,
-                            isEnd = true
-                        };
-                        break;
-                    case "no":
-                        cells[i, j] = new Cell()
-                        {
-                            stepCost = -1
-                        };
-                        break;
-                    default:
-                        cells[i, j] = new Cell()
-                        {
-                            stepCost = Convert.ToInt32(costs[_map[i, j]]),
-                            distanceToEnd = (Math.Abs(i - ii) + Math.Abs(j - jj)) * minStepCost
-                        };
-                        break;
-                }
-            }
-        }
-
-        return cells;
+        newMap = map;
     }
 
-    public static List<Point> GenerateWay(Cell[,] cells)
+    public static int[,] GetEnableArea(int[,] map, int x, int y, int radius, out Cell[,] cells)
+    {
+        int[,] miniMap;
+        int x1, y1;
+        GetPieceOfMap(map, x, y, radius, out map, out miniMap, out x1, out y1);
+        cells = PathGenerater.GetCellsFromMap(miniMap);
+        cells = Optimaze(cells);
+        for (int i = 0; i < miniMap.GetLength(0); i++)
+        {
+            for (int j = 0; j < miniMap.GetLength(1); j++)
+            {
+                if (cells[i, j].cellCost > radius * 10 || cells[i, j].cellCost == -1)
+                    map[i + x1, j + y1] = 1;
+            }
+        }
+
+        return map;
+    }
+    public static Cell[,] Optimaze(Cell[,] cells)
     {
         int ii = 0;
         int jj = 0;
-        //string a1, a2, a3, a4;
+        string a1, a2, a3, a4;
         StartPositionFromCells(cells, out ii, out jj);
-        while (!cells[ii, jj].isEnd)
+        while (!cells[ii, jj].isCheckeed)
         {
-            //a1 = "";
-            //a2 = "";
-            //a3 = "";
-            //a4 = "";
-            //for (int i = 0; i < cells.GetLength(0); i++)
-            //{
-            //    for (int j = 0; j < cells.GetLength(1); j++)
-            //    {
-            //        a1 += cells[i, j].cellCost.ToString() + " | ";
-            //        a2 += cells[i, j].isCheckeed.ToString() + " | ";
-            //        a4 += cells[i, j].stepCost.ToString() + " | ";
-            //    }
-            //    a1 += '\n';
-            //    a2 += '\n';
-            //    a4 += '\n';
-            //}
-            //a3 = a1 + '\n' + a2 + '\n' + a4;
-            if (cells[ii, jj].isCheckeed)
+            a1 = "";
+            a2 = "";
+            a3 = "";
+            a4 = "";
+            for (int i = 0; i < cells.GetLength(0); i++)
             {
-                return null;
+                for (int j = 0; j < cells.GetLength(1); j++)
+                {
+                    a1 += cells[i, j].cellCost.ToString() + " | ";
+                    a2 += cells[i, j].isCheckeed.ToString() + " | ";
+                    a4 += cells[i, j].stepCost.ToString() + " | ";
+                }
+                a1 += '\n';
+                a2 += '\n';
+                a4 += '\n';
             }
+            a3 = a1 + '\n' + a2 + '\n' + a4;
             cells[ii, jj].isCheckeed = true;
             if (ii > 0)
             {
@@ -179,42 +149,25 @@ public static class PathGenerater
                     cells[ii, jj + 1].prevCell = new Point(ii, jj);
                 }
             }
-            int minCost = -1;
-            int minDistance = 0;
-            for (int i = 0; i < cells.GetLength(0); i++)
+            int min = -1;
+            for (int i = 0;i < cells.GetLength(0); i++)
             {
-                for (int j = 0; j < cells.GetLength(1); j++)
+                for (int j = 0;j < cells.GetLength(1); j++)
                 {
                     if (cells[i, j].cellCost != -1 && !cells[i, j].isCheckeed)
                     {
-                        if (minCost == -1 || 
-                            (
-                                (minCost > (cells[i, j].cellCost + cells[i, j].distanceToEnd) || (minCost == (cells[i, j].cellCost + cells[i, j].distanceToEnd) && minDistance > cells[i, j].distanceToEnd)))
-                            )
+                        if (min == -1 || min > cells[i, j].cellCost)
                         {
                             ii = i;
                             jj = j;
-                            minCost = cells[i, j].cellCost + cells[i, j].distanceToEnd;
-                            minDistance = cells[i, j].distanceToEnd;
+                            min = cells[i, j].cellCost;
                         }
                     }
                 }
             }
         }
 
-        List<Point> points = new List<Point>() { new Point(ii, jj)};
-        while (cells[ii, jj].cellCost != 0)
-        {
-            if (cells[ii, jj].prevCell.X == ii && cells[ii, jj].prevCell.Y == jj)
-            {
-                return null;
-            }
-            Point point = cells[ii, jj].prevCell;
-            points.Add(point);
-            ii = point.X;
-            jj = point.Y;
-        }
-        return points;
+        return cells;
     }
 
     static void StartPositionFromCells(Cell[,] cells, out int ii, out int jj)
@@ -233,15 +186,5 @@ public static class PathGenerater
         }
         ii = 0;
         jj = 0;
-    }
-
-    public class Cell
-    {
-        internal int stepCost;
-        internal bool isEnd = false;
-        internal int distanceToEnd;
-        internal int cellCost = -1;
-        internal bool isCheckeed = false;
-        internal Point prevCell;
     }
 }
--- Assets/Scripts/PathDrawer.cs
+++ Assets/Scripts/PathDrawer.cs
@@ -2,16 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Reflection;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PathDrawer : MonoBehaviour
 {
+    public GameObject ActiveZone;
+    public Text roundText;
+    public int DistanceToMove;
+    int remainingSteps;
+    int round = 0;
     Point lastPoint = new Point(-1, 0);
-    Point mouseClick = new Point(1, 1);
+    Point mouseClick;
     MeshFilter mesh;
+    MeshFilter zoneMesh;
+    int[,] AreaMap;
+    PathGenerater.Cell[,] cells;
     private void Start()
     {
         mesh = GetComponent<MeshFilter>();
+        zoneMesh = ActiveZone.GetComponent<MeshFilter>();
+        remainingSteps = DistanceToMove;
     }
     void Update()
     {
@@ -27,21 +40,33 @@ public class PathDrawer : MonoBehaviour
             List<int> triangles = new List<int>();
             if (pointX >= 0 && pointY >= 0 && pointX + 0.8f > point.x && pointY + 0.8f > point.y && GlobalData.Map[pointX, pointY] != 1)
             {
-                if (lastPoint.X != pointX || lastPoint.Y != pointY)
+                if (AreaMap != null)
                 {
-                    lastPoint = new Point(pointX, pointY);
-                    int[,] newMap = new int[GlobalData.Map.GetLength(0), GlobalData.Map.GetLength(1)];
-                    newMap = (int[,])GlobalData.Map.Clone();
-                    newMap[mouseClick.X, mouseClick.Y] = 2;
-                    newMap[pointX, pointY] = 3;
-                    var cells = PathGenerater.GetCellsFromMap(newMap);
-                    var path = PathGenerater.GenerateWay(cells);
-                    mesh.mesh = MeshGeneration.GenerateLine(path);
-                    Debug.Log(mouseClick);
+                    if ( AreaMap[pointX, pointY] != 1)
+                    {
+                        if ((lastPoint.X != pointX || lastPoint.Y != pointY) && (pointX != mouseClick.X || pointY != mouseClick.Y))
+                        {
+                            lastPoint = new Point(pointX, pointY);
+                            int[,] newMap = new int[GlobalData.Map.GetLength(0), GlobalData.Map.GetLength(1)];
+                            newMap = (int[,])GlobalData.Map.Clone();
+                            newMap[mouseClick.X, mouseClick.Y] = 2;
+                            newMap[pointX, pointY] = 3;
+                            var cells = PathGenerater.GetCellsFromMap(newMap);
+                            var path = PathGenerater.GenerateWay(cells);
+                            mesh.mesh = MeshGeneration.GenerateLine(path);
+                        }
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            SomeActionWhenClick(pointX, pointY);
+                        }
+                    }
                 }
-                if (Input.GetMouseButtonDown(0))
+                else
                 {
-                    mouseClick = new Point(pointX, pointY);
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        SomeActionWhenClick(pointX, pointY);
+                    }
                 }
             }
             else
@@ -59,5 +84,38 @@ public class PathDrawer : MonoBehaviour
             mesh.mesh = new Mesh();
             lastPoint.X = -2;
         }
+    }
+
+    void SomeActionWhenClick(int pointX, int pointY)
+    {
+        if (cells != null)
+        {
+            int x = pointX + (remainingSteps < mouseClick.X ? remainingSteps - mouseClick.X : 0);
+            int y = pointY + (remainingSteps < mouseClick.Y ? remainingSteps - mouseClick.Y : 0);
+            remainingSteps -= cells[x, y].cellCost / 10;
+            if (remainingSteps == 0)
+            {
+                roundText.text = (++round).ToString();
+                remainingSteps = DistanceToMove;
+            }
+        }
+        int[,] newMap = new int[GlobalData.Map.GetLength(0), GlobalData.Map.GetLength(1)];
+        newMap = (int[,])GlobalData.Map.Clone();
+        AreaMap = AreaGenerater.GetEnableArea(newMap, pointX, pointY, remainingSteps, out cells);
+        zoneMesh.mesh = MeshGeneration.GenerateMeshFromMap(AreaMap);
+        lastPoint.X = -1;
+        mouseClick = new Point(pointX, pointY);
+    }
+
+    public void IfNewGeneration()
+    {
+        mesh.mesh = new Mesh();
+        cells = null;
+        remainingSteps = DistanceToMove;
+        round = 0;
+        roundText.text = (++round).ToString();
+        lastPoint = new Point(-1, 0);
+        zoneMesh.mesh = new Mesh();
+        AreaMap = null;
     }
 }
